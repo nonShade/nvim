40337e45f4a7442f55039c709e196b26abb20b40
nonShade <b.sanmartin06@ufromail.cl>
2025-07-23 00:10:36 -0400

    feat: change colorscheme and add hardtime plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     require("rose-pine").setup({})
 
-    -- vim.cmd("colorscheme rose-pine")
+    vim.cmd("colorscheme rose-pine")
     -- vim.cmd("colorscheme rose-pine-main")
-    vim.cmd("colorscheme rose-pine-moon")
+    -- vim.cmd("colorscheme rose-pine-moon")
     -- vim.cmd("colorscheme rose-pine-dawn")
   end,
 }
