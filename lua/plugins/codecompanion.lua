90066ca108ce4330b7008d020a87076ed2dd5569
nonShade <b.sanmartin06@ufromail.cl>
2025-07-25 02:27:17 -0400

    feat: AI plugins

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,113 @@
+return {
+  "olimorris/codecompanion.nvim",
+  init = function()
+    vim.cmd([[cab cc CodeCompanion]])
+
+    local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "CodeCompanionInlineFinished",
+      group = group,
+      callback = function(request)
+        vim.lsp.buf.format({ bufnr = request.buf })
+      end,
+    })
+  end,
+  cmd = {
+    "CodeCompanion",
+    "CodeCompanionActions",
+    "CodeCompanionChat",
+    "CodeCompanionCmd",
+  },
+  keys = {
+    { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "AI Toggle [C]hat" },
+    { "<leader>an", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "AI [N]ew Chat" },
+    { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "AI [A]ction" },
+    { "ga", "<cmd>CodeCompanionChat Add<CR>", mode = { "v" }, desc = "AI [A]dd to Chat" },
+    -- prompts
+    { "<leader>ae", "<cmd>CodeCompanion /explain<cr>", mode = { "v" }, desc = "AI [E]xplain" },
+  },
+  config = true,
+  opts = {
+    adapters = {
+      copilot_4o = function()
+        return require("codecompanion.adapters").extend("copilot", {
+          schema = {
+            model = {
+              default = "gpt-4o",
+            },
+          },
+        })
+      end,
+      copilot_41 = function()
+        return require("codecompanion.adapters").extend("copilot", {
+          schema = {
+            model = {
+              default = "gpt-4.1",
+            },
+          },
+        })
+      end,
+      copilot_gemini_25_pro = function()
+        return require("codecompanion.adapters").extend("copilot", {
+          schema = {
+            model = {
+              default = "gemini-2.5-pro",
+            },
+          },
+        })
+      end,
+    },
+    display = {
+      diff = {
+        enabled = true,
+        close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+        layout = "vertical", -- vertical|horizontal split for default provider
+        opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+        provider = "default", -- default|mini_diff
+      },
+      chat = {
+        window = {
+          position = "right", -- right|left|bottom
+        },
+      },
+    },
+    strategies = {
+      inline = {
+        keymaps = {
+          accept_change = {
+            modes = { n = "ga" },
+            description = "Accept the suggested change",
+          },
+          reject_change = {
+            modes = { n = "gr" },
+            description = "Reject the suggested change",
+          },
+        },
+      },
+      chat = {
+        keymaps = {
+          send = {
+            modes = { n = "<CR>", i = "<C-s>" },
+          },
+          close = {
+            modes = { n = "<C-c>", i = "<C-c>" },
+          },
+          -- Add further custom keymaps here
+        },
+        adapter = "copilot",
+        roles = {
+          ---The header name for the LLM's messages
+          ---@type string|fun(adapter: CodeCompanion.Adapter): string
+          llm = function(adapter)
+            return "AI (" .. adapter.formatted_name .. ")"
+          end,
+
+          ---The header name for your messages
+          ---@type string
+          user = "Shade",
+        },
+      },
+    },
+  },
+}
